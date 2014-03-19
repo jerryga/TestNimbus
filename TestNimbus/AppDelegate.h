996fc1bf651cbe0d14e460d7a7debd6784ccaa78
Static Ga <staticga@gmail.com>
2014-03-19 10:51:34 +0800

    Initial Commit

---
+++ TestNimbus/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TestNimbus
+//
+//  Created by Static Ga on 14-3-19.
+//  Copyright (c) 2014年 Static Ga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TestNimbus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestNimbus
+//
+//  Created by Static Ga on 14-3-19.
+//  Copyright (c) 2014年 Static Ga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
